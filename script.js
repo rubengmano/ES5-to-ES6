fd757d4511dd29727e284e1cda9a2ca5cdf04608
Rúben Mano <rumcmano@gmail.com>
2019-05-16 16:52:49 +0300

    let and const (block scoped)

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
-// Hello
+// New ways of declaring variables
+
+// ES5
+var name5 = 'Ruben Mano';
+var age5 = 22;
+name5 = 'Cintia Pasto';
+console.log(name5);
+
+// ES6
+// In ES6 var is no longer used and it's replaced by const if the variable does not change
+// or let if the variable can change
+const name6 = 'Ruben Mano';
+let age6 = 22;
+// The next line has an error if the variable name6 is declared as const
+// name6 = 'Cintia Pasto';
+console.log(name6);
+
+// ES5
+function driversLicense5(passedTest) {
+    if (passedTest){
+        var firstName = 'John Smith';
+        var yearOfBirth = 1990;
+    }
+    // this works because the variables are function scope
+    console.log(firstName + ', born in ' + yearOfBirth + ' is now officially allowed to drive a car.');
+
+}
+
+driversLicense5(true);
+
+// ES6
+function driversLicense6(passedTest) {
+    // The Problem 
+    /*if (passedTest){
+        let firstName = 'John Smith';
+        const yearOfBirth = 1990;
+
+    }
+    // this does not work because the variables are block scoped
+    // A block is all the code wrapped within the curly braces
+    console.log(firstName + ', born in ' + yearOfBirth + ' is now officially allowed to drive a car.');
+    */
+
+    // The solution
+    // Declare the variables in the scope of the console.log();
+    let firstName;
+    // The const is declared outside
+    const yearOfBirth = 1990;
+    if (passedTest){
+        firstName = 'John Smith';
+
+    }
+    // this does not work because the variables are block scoped
+    // A block is all the code wrapped within the curly braces
+    console.log(firstName + ', born in ' + yearOfBirth + ' is now officially allowed to drive a car.');
+    
+}
+
+driversLicense6(true);
